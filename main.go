f602bb9030fb9215ba6494c4f6a2a627c5cc2f7c
Aries091 <97426155+Aries091@users.noreply.github.com>
2025-04-19 20:07:58 +0545

    working in movements

--- main.go
+++ main.go
@@ -1,35 +1,64 @@
+// main.go
 package main
 
 import (
-	"bytes"
 	"image"
 	_ "image/png"
 	"log"
+	"os"
 
 	"github.com/hajimehoshi/ebiten/v2"
 )
 
-var ghostImageData []byte
-
 var ghostImage *ebiten.Image
 
 func init() {
-	img, _, err := image.Decode(bytes.NewReader(ghostImageData))
+	file, err := os.Open("ghost.png")
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("Failed to open image file:", err)
+	}
+	defer file.Close()
+
+	img, _, err := image.Decode(file)
+	if err != nil {
+		log.Fatal("Failed to decode image:", err)
 	}
 	ghostImage = ebiten.NewImageFromImage(img)
 }
 
-type Game struct{}
+type Game struct {
+	y         float64
+	velocityY float64
+}
+
+const (
+	gravity   = 0.4
+	jumpForce = -9.5
+	groundY   = 350
+	scale     = 0.5
+	ghostX    = 100
+)
 
 func (g *Game) Update() error {
+	if ebiten.IsKeyPressed(ebiten.KeySpace) && g.y >= groundY {
+
+		g.velocityY = jumpForce
+	}
+
+	g.velocityY += gravity
+	g.y += g.velocityY
+
+	if g.y > groundY {
+		g.y = groundY
+		g.velocityY = 0
+	}
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
 	op := &ebiten.DrawImageOptions{}
-	op.GeoM.Translate(100, 100) // Position of the ghost
+	op.GeoM.Scale(0.25, 0.25)
+	op.GeoM.Translate(ghostX, g.y)
 	screen.DrawImage(ghostImage, op)
 }
 
@@ -39,7 +68,7 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
 
 func main() {
 	ebiten.SetWindowSize(640, 480)
-	ebiten.SetWindowTitle("Ghost Game")
+	ebiten.SetWindowTitle("Ghost Game - File Load")
 	if err := ebiten.RunGame(&Game{}); err != nil {
 		log.Fatal(err)
 	}
